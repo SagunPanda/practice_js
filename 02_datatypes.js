dff5a9a792c803063ff36c72cf0fa26f92950b33
Sagun2001 <pandasagun@gmail.com>
2024-01-04 11:13:32 +0530

    about datatypes

---
+++ 02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3+3) // we are using node.js not browser
+
+
+let name= "sagun"
+let age = 23
+let isLoggedIn= false
+let state
+
+//number => 2 to power 53
+//bigint
+//string =>""
+//boolean => true/false
+//null =>standalone vale
+//undefined
+//symbol => uniqe
+
+console.log(typeof null) // o/p => object
+console.log(typeof undefined) //  o/p => undefined
