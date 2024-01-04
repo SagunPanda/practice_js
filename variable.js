f5115874f1b690f62eb28a086071033efb1d9944
Sagun2001 <pandasagun@gmail.com>
2024-01-04 10:43:55 +0530

    about variables

---
+++ variable.js
@@ -0,0 +1,19 @@
+const accountId =1234
+let accountEmai= "pandasagun@gmail.com"
+var accountPassword= "sriram"
+accountCity='berhampur'
+
+//accountId= 3456 //not allowed
+accountEmail= "sagunpanda@gmail.com"
+accountPassword= "5678"
+accountCity='bbsr'
+let accountState;
+
+//console.log(accountId);
+console.table([accountEmail,accountPassword,accountCity,accountState])
+
+/*
+prefer not use var bcz for issue in block scope and functional scope
+if we declare a variable but not declare the values then it shows undefined
+
+*/
