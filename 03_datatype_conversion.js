721e60f66018e2affbb6937c017601e8e34297a9
Sagun2001 <pandasagun@gmail.com>
2024-01-06 17:37:39 +0530

    fst commit

---
+++ 03_datatype_conversion.js
@@ -0,0 +1,15 @@
+let score= 33
+console.log(typeof score);
+
+let score2 ="33"
+console.log(typeof score2);
+// i want to convert it in number
+let valueInNumber=Number(score2)
+console.log(typeof valueInNumber);
+
+let score3 ="33abc"
+console.log(typeof score3);
+let valueInNumber2=Number(score3)
+console.log(typeof valueInNumber2);
+console.log(valueInNumber2);
+
